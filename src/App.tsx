23ef09b8a57098d86b8160fe49d2a5851a8267a7
matt-mission <matt@mission.dev>
2022-01-31 18:55:33 -0500

    image fix attempt

--- src/App.tsx
+++ src/App.tsx
@@ -219,7 +219,7 @@ function App() {
         {MISSION_MESSAGE}
       </button>
 
-      <image src="/logo.svg" height="25px"></image>
+      <image [src]="/logo.svg" height="25px"></image>
 
       <Alert message={NOT_ENOUGH_LETTERS_MESSAGE} isOpen={isNotEnoughLetters} />
       <Alert
