d3810454114e794a18be28aec2821316db7548bb
matt-mission <matt@mission.dev>
2022-01-31 18:19:55 -0500

    Added new colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mission-blue': '#3ba3e7',
+        'mission-purple': '#554cc5',
+        'mission-dark-purple': '#26225b',
+        'mission-coral': '#ff7e75',
+        'mission-grey': '#535269',
+        'mission-dark-grey': '#27263a',
+        'mission-pink': '#e64b91,
+      },
+    },
   },
   plugins: [],
 }
